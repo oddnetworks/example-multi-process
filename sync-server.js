eed784c701c8ffb99a818f1697569b6488d80114
Erik Straub <erik.straub@oddnetworks.com>
2016-05-18 14:03:24 -0400

    Adds sync server

---
+++ sync-server.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const redis = require('redis');
+const oddworks = require('@oddnetworks/oddworks');
+
+const StoresUtils = oddworks.storesUtils;
+const ServicesUtils = oddworks.servicesUtils;
+
+const oddcast = require('oddcast');
+const bus = oddcast.bus();
+
+const FIVE_MINUTES = 5 * 60 * 1000;
+const SYNC_INTERVAL = process.env.SYNC_INTERVAL || FIVE_MINUTES;
+
+const VIMEO_API_TOKEN = process.env.VIMEO_API_TOKEN;
+const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+
+const redisClient = redis.createClient(REDIS_URL);
+const seedData = require('./data/seed');
+
+// Initialize oddcast for events, commands, requests
+bus.events.use({}, oddcast.inprocessTransport());
+bus.commands.use({}, oddcast.inprocessTransport());
+bus.requests.use({}, oddcast.inprocessTransport());
+
+// Initialize stores
+module.exports = StoresUtils.load(bus,
+	[
+		{
+			store: oddworks.stores.redis,
+			options: {redis: redisClient, types: ['channel', 'platform', 'collection', 'promotion', 'video', 'view']}
+		},
+		{
+			store: oddworks.stores.redisSearch,
+			options: {redis: redisClient, types: ['collection', 'video']}
+		}
+	])
+	// Seed the stores with channel and platform data
+	.then(() => {
+		return seedData(bus);
+	})
+	.then(() => {
+		// Initialize services
+		return ServicesUtils.load(bus, [
+			{
+				service: oddworks.services.catalog,
+				options: {}
+			},
+			{
+				service: oddworks.services.sync,
+				options: {
+					interval: SYNC_INTERVAL,
+					providers: [
+						oddworks.services.sync.providers.vimeo({token: VIMEO_API_TOKEN})
+					]
+				}
+			}
+		]);
+	})
+	.catch(err => oddworks.logger.error(err.stack));
