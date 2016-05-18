112358fd72cb2e6f435981e73352dc38b246b2e9
Erik Straub <erik.straub@oddnetworks.com>
2016-05-18 14:29:20 -0400

    Adds logger configuration to sync-server

--- sync-server.js
+++ sync-server.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const redis = require('redis');
+const Promise = require('bluebird');
+const winston = require('winston');
 const oddworks = require('@oddnetworks/oddworks');
 
 const StoresUtils = oddworks.storesUtils;
@@ -9,7 +11,10 @@ const ServicesUtils = oddworks.servicesUtils;
 const oddcast = require('oddcast');
 const bus = oddcast.bus();
 
+const ENVIRONMENT = process.env.NODE_ENV || 'development';
+const LOG_LEVEL = ENVIRONMENT === 'production' ? 'info' : 'debug';
 const FIVE_MINUTES = 5 * 60 * 1000;
+const UTC_OFFSET = 0;
 const SYNC_INTERVAL = process.env.SYNC_INTERVAL || FIVE_MINUTES;
 
 const VIMEO_API_TOKEN = process.env.VIMEO_API_TOKEN;
@@ -23,18 +28,38 @@ bus.events.use({}, oddcast.inprocessTransport());
 bus.commands.use({}, oddcast.inprocessTransport());
 bus.requests.use({}, oddcast.inprocessTransport());
 
-// Initialize stores
-module.exports = StoresUtils.load(bus,
-	[
-		{
-			store: oddworks.stores.redis,
-			options: {redis: redisClient, types: ['channel', 'platform', 'collection', 'promotion', 'video', 'view']}
-		},
-		{
-			store: oddworks.stores.redisSearch,
-			options: {redis: redisClient, types: ['collection', 'video']}
-		}
-	])
+function setupLogger() {
+	oddworks.logger.configure({
+		transports: [
+			new winston.transports.Console({
+				level: LOG_LEVEL,
+				colorize: true,
+				timestamp() {
+					return new Date().format('YYYY-MM-DDThh:mm:ss.SSSZ', UTC_OFFSET);
+				},
+				handleExceptions: true
+			})
+		]
+	});
+	return Promise.resolve(true);
+}
+
+// Setup logger
+module.exports = setupLogger()
+	.then(() => {
+		// Initialize stores
+		StoresUtils.load(bus,
+			[
+				{
+					store: oddworks.stores.redis,
+					options: {redis: redisClient, types: ['channel', 'platform', 'collection', 'promotion', 'video', 'view']}
+				},
+				{
+					store: oddworks.stores.redisSearch,
+					options: {redis: redisClient, types: ['collection', 'video']}
+				}
+			])
+	})
 	// Seed the stores with channel and platform data
 	.then(() => {
 		return seedData(bus);
